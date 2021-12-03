51f857adee6db40e2687ce429a48619329da2bb7
Marcus Schnake <marcus.schnake@gmail.com>
2021-12-03 00:04:36 -0500

    Base64 code working

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Auth from './auth/Auth';
 import Sitebar from './home/Navbar'
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import PostCreate from './posts/PostCreate';
 
 function App() {
   return (
     <div className="App">
       <Sitebar />
+      <PostCreate/>
     </div>
   );
 }
+
+
 
 export default App;
