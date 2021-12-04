ea5901b542e940c0ed97b71f3571075c184fd009
LA-Miller <lamiller516@gmail.com>
2021-12-04 13:57:56 -0500

    Changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import Auth from './auth/Auth';
-import Sitebar from './home/Navbar'
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
-import PostCreate from './posts/PostCreate';
+import React, { useState, useEffect } from "react";
+import Auth from "./auth/Auth";
+import Sitebar from "./home/Navbar";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import PostCreate from "./posts/PostCreate";
 
 function App() {
-  const [ sessionToken, setSessionToken ] = useState('');
+  const [sessionToken, setSessionToken] = useState("");
 
   useEffect(() => {
-    setSessionToken(localStorage.getItem('token'));
-  }, [])
+    setSessionToken(localStorage.getItem("token"));
+  }, []);
 
   const updateToken = (newToken) => {
-    localStorage.setItem('token', newToken);
+    localStorage.setItem("token", newToken);
     setSessionToken(newToken);
-  }
+  };
 
   const clearToken = () => {
     localStorage.clear();
-    setSessionToken('');
-  }
+    setSessionToken("");
+  };
 
   // const protectedViews = () => {
   //   return (sessionToken === localStorage.getItem('token') ? <WorkoutIndex token={sessionToken} /> : <Auth updateToken={updateToken}/>)
@@ -28,14 +28,10 @@ function App() {
 
   return (
     <div className="App">
-    
-      <Sitebar clickLogout={clearToken}/>
-      <PostCreate/>
-    
+      <Sitebar clickLogout={clearToken} />
+      <PostCreate />
     </div>
   );
 }
-
-
 
 export default App;
