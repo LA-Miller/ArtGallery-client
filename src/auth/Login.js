c6ffee34ef78ef7f288411bc04ae55ca4409399a
Luke Miller <lamiller516@gmail.com>
2021-12-17 00:11:08 -0500

    Changed fetch Urls to Heroku

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -8,7 +8,7 @@ const Login = (props) => {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    fetch("http://localhost:3333/user/login", {
+    fetch("https://lam-art-gallery-server.herokuapp.com/user/login", {
       method: "POST",
       body: JSON.stringify({
         user: { email: email, password: password },
